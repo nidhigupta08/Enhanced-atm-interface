311db6eaa7ef4331dc85a65bd6265d7d568b6c40
Nidhi <guptanidhi8000@gmail.com>
2024-07-17 23:13:18 +0530

    Add model classes for User

---
+++ src/main/java/updated/atminterface/models/User.java
@@ -0,0 +1,33 @@
+package updated.atminterface.models;
+
+public class User {
+    private String userId;
+    private String pin;
+    private double balance;
+    
+    // Constructor, getters, setters
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
