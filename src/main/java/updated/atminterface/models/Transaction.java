0ec6977d56199ed314824975d7d1eb7c2c2be11c
Nidhi <guptanidhi8000@gmail.com>
2024-07-17 23:13:48 +0530

    Add model classes for Transaction

---
+++ src/main/java/updated/atminterface/models/Transaction.java
@@ -0,0 +1,53 @@
+package updated.atminterface.models;
+
+import java.sql.Timestamp;
+
+public class Transaction {
+    private int transactionId;
+    private String userId;
+    private String type;
+    private double amount;
+    private Timestamp date;
+    
+    // Constructor, getters, setters
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+}
